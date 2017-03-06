9fcfd113fbc5ecad8dbe81d8661864b3ae2182f3
cpxjl <616487222@qq.com>
2017-03-06 23:44:42 +0800

    增加和值筛选,ui优化

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ handleCheckStart=()=>{
     allNums=[];
     checkNumsLength=$("input[name='checkNumers']:checked").val();
 
-    checkNumsLength=2;
+    checkNumsLength=6;
     var validNumsArr=[];
-    for(let i=1;i<=37;i++){
+    for(let i=1;i<=36;i++){
         allNums.push(i);
     }
     $('input[name="notInNums"]:checked').each(function(){
@@ -34,34 +34,48 @@ handleCheckStart=()=>{
         //    isValid=test.checkIsContinuum();
         //}
         //奇数排除
-        if($("input[name='oddRadio']:checked").val() && isValid){
+        if($("input[name='oddNumbres']:checked").val() && isValid){
             isValid=test.checkOddNumbers(parseInt($("input[name='oddNumbres']:checked").val()));
 
         }
         //偶数
-        if($("input[name='evenRadio']:checked").val()  && isValid){
+        if($("input[name='evenNumbres']:checked").val()  && isValid){
             isValid=test.checkEvenNumbers(parseInt($("input[name='evenNumbres']:checked").val()));
         }
         //尾数筛选
         if(isValid){
-            $('input[name="notInGroup"]:checked').each(function(){
-                let sameLength=parseInt($(this).val());
-                let checkNumsArr=[];
-                $('input[name="notInNums_'+sameLength+'"]:checked').each(function(){
-                    checkNumsArr.push(parseInt($(this).val()))
-                })
-                isValid=test.checkLastNumber(sameLength,checkNumsArr)
-                if(!isValid){
-                    return false;
+            let checkNumsMap={};
+            $('.notInNums:checked').each(function(){
+                let notInNumsValue=$(this).val().split("-");
+                let sameLength=notInNumsValue[0];
+                if(!checkNumsMap[sameLength]){
+                    checkNumsMap[sameLength]=[]
                 }
+                let checkNums=notInNumsValue[1];
+                checkNumsMap[sameLength].push(parseInt(checkNums));
             })
+            for(let checkNum in checkNumsMap){
+                isValid=test.checkLastNumber(checkNum,checkNumsMap[checkNum])
+                if(!isValid){
+                    break;
+                }
+            }
+        }
+        if(isValid && $("#largerThan").val()){
+            isValid=!test.checkSumNumbers($("#largerThan").val())
+        }
+        if(isValid && $("#smallerThan").val()){
+            isValid=test.checkSumNumbers($("#smallerThan").val()-1)
         }
 
+
+        //分段筛选
         if(isValid){
-            $('input[name="paragraphGroup"]:checked').each(function(){
-                let startValue=parseInt($(this).val());
-                let paragraphLength=parseInt($('input[name="paragraphNums_'+startValue+'"]:checked').val());
-                isValid=test.checkContinuumParagraph(startValue,paragraphLength);
+            $(".check-paragraph:checked").each(function(){
+                let paragraphValue=$(this).val().split("-");
+                let startValue=parseInt(paragraphValue[0]);
+                let paragraphLength=parseInt(paragraphValue[1]);
+                isValid=test.checkContinuumParagraph(startValue,paragraphLength)
                 if(!isValid){
                     return false;
                 }
@@ -70,6 +84,7 @@ handleCheckStart=()=>{
 
         if(isValid) {
             console.log(numsArr)
+            //console.log(numsArr,isValid,$("#largerThan").val(),test.checkSumNumbers($("#largerThan").val()))
         }
         numsArr=handleAddNumByIndex(numsArr,(checkNumsLength-1))
         if(!numsArr){
