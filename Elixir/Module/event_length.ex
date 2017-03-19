1bb8a66a6a1400546d5033fa9acfade17a7d8358
谢静龄 <xiejingling@ones.ai>
2017-03-19 12:33:42 +0800

    尝试去掉括号

--- Elixir/Module/event_length.ex
+++ Elixir/Module/event_length.ex
@@ -6,27 +6,23 @@ defmodule EventLength do
   end
 
   def checkIsEvenOverLengthByIndex(numbers,length,count,index) when index>=(tuple_size numbers)do
-    IO.puts '结束了且应该没超出'
+    true
   end
 
   def checkIsEvenOverLengthByIndex(numbers,length,count,index) when count>=length do
-       IO.puts '超出了'
+     false
   end
 
   def checkIsEvenOverLengthByIndex(numbers,length,count,index) do
-       if checkIsEven(elem(numbers,index)) do
-        checkIsEvenOverLengthByIndex(numbers,length,count+1,index+1)
+       if (checkIsEven elem numbers,index) do
+        checkIsEvenOverLengthByIndex numbers,length,count+1,index+1
        else
-        checkIsEvenOverLengthByIndex(numbers,length,count,index+1)
+        checkIsEvenOverLengthByIndex numbers,length,count,index+1
        end
   end
 
-
-
-
-
   def checkIsEvenOverLength(numbers,length) do
     count=0;
-    checkIsEvenOverLengthByIndex(numbers,length,count,0);
+    checkIsEvenOverLengthByIndex numbers,length,count,0;
   end
 end
